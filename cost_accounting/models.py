d7d59d83fe8852b2d9bb01e2e34b93a7106d4b0f
1996p <dursinear@gmail.com>
2022-07-12 14:53:16 +0700

    created models

---
+++ cost_accounting/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Spending(models.Model):
+    """Описывает расходы, принадлежащие к какой-то категории товаров"""
+    payer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Плательщик')
+    amount = models.PositiveIntegerField(verbose_name='Потраченная сумма')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория товара')
+
+    class Meta:
+        db_table = 'spending'
+
+    def __str__(self):
+        return f'{self.payer} -> {self.category} | {self.amount}'
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Название группы')
+    members = models.ManyToManyField(User, verbose_name="Участники группы")
+
+    class Meta:
+        db_table = 'user_group'
+
+    def __str__(self):
+        return f'{self.name} | ({self.members.count()})'
+
+
+class Category(models.Model):
+    """Описывает категории товаров, на которые человек тратит деньги"""
+    title = models.CharField(max_length=50, verbose_name='Название категории')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name="Трата на группу", blank=True, null=True)
+
+    class Meta:
+        db_table = 'spending_category'
+
+    def __str__(self):
+        return self.title
